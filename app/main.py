dbdc3f1b0e18d150fa1b5bdf68d90c8b47d564cf
Cleiton Pereira <cleiton.gspereira@outlook.com>
2026-02-07 01:36:19 -0300

    fix: atualizando a logica do main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.api.v1.router import api_router
 from app.core.database import Base, engine
 
+
 def create_app() -> FastAPI:
     app = FastAPI(title="FastAPI Auth Service")
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[
+            "http://localhost:3000",
+            "https://twmanager-frontend.up.railway.app",
+        ],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
     app.include_router(api_router)
     return app
+
 
 Base.metadata.create_all(bind=engine)
 
